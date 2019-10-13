4a90c0781323c55d53e6bc4fca814fe10b8d3d71
outfitfess <56522263+outfitfess@users.noreply.github.com>
2019-10-14 06:14:42 +0700

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+   consumer_key:uUoam0a0TsYonmnuhsoh6oXkb process.env.C_KEY,
+   consumer_secret:A6kFCcoC2tUfstN3TRNULe7ne5qt4z75w9JbB1IW30tEVXn4ro process.env.C_SECRET,
+   access_token:1899927169-KHIaMltlvdgmd5uhYYyoK92yL1NJ7YIMnDLh2MX process.env.A_TOKEN,
+   access_token_secret:LHXNwHqX2JugQlYE3RmNh7kQXsduSMei5lYprFJfBtFHI process.env.A_SECRET,
+   timeout_ms: 60*1000
+}
